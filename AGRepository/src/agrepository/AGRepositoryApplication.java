8de6019ea414fe3bf61f68160adff1fb2a38fa55
haattila@gmail.com <haattila@gmail.com@a7a22c89-8f85-767e-4947-e7c7c3c3236c>
2010-10-21 17:48:19 +0000

    Polishing initial release

--- AGRepository/src/agrepository/AGRepositoryApplication.java
+++ AGRepository/src/agrepository/AGRepositoryApplication.java
@@ -1,15 +1,17 @@
 package agrepository;
 
 import com.vaadin.Application;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window;
 
 public class AGRepositoryApplication extends Application {
-	@Override
-	public void init() {
-		Window mainWindow = new Window("AG Repository Application");
-		Label label = new Label("Hello Vaadin user");
-		mainWindow.addComponent(label);
-		setMainWindow(mainWindow);
-	}
+   private static final long serialVersionUID = -6919355554494428313L;
 
+   @Override
+   public void init() {
+      Window mainWindow = new Window("Application Generator Repository");
+      Label label = new Label("Hello to Application Generator Repository!");
+      mainWindow.addComponent(label);
+      setMainWindow(mainWindow);
+   }
 }
